c957eb26dcf85b10f0b432e4a5b7a42c31bb204e
Nicolletta Eneji <nicoleeneji@gmail>
2026-02-04 13:00:29 +0100

    made add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id ="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -58,4 +58,30 @@
     const productsGrid = document.querySelector('.js-products-grid');
     productsGrid.innerHTML = productsHTML;
 
-    console.log(productsHTML);
+    document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+        button.addEventListener('click', ()=> {
+          const productId = button.dataset.productId;
+
+          let matchingItem;
+
+          cart.forEach((item) => {
+            if (productId=== item.productId) {
+              matchingItem = item;
+            }
+          });
+
+          if (matchingItem) {
+            matchingItem.quantity += 1;
+          }else{
+
+            cart.push({
+              productId: productId,
+              quantity: 1
+            });
+          }
+
+          console.log(cart);
+        });
+    });
+
+    

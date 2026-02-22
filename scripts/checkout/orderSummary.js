88f74d92da59a4c7e527d98f3ef2c65c4d8808b6
Nicolletta Eneji <nicoleeneji@gmail>
2026-02-22 09:34:55 +0100

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,6 +2,7 @@ import { cart, removeFromCart,getCartQuantity,updateQuantity, updateDeliveryOpti
 import { products, getProduct } from "../../data/products.js"; 
 import { formatCurrency } from "../utils/money.js";
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js"; 
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 // console.log(dayjs());
@@ -132,13 +133,14 @@ export function renderOrderSummary(){
     
             const container = document.querySelector(`.js-cart-item-container-${productId}`)
             // console.log(container);
-    
-                
-                container.remove();
-                const newQuantity = getCartQuantity();
-                const checkoutLink = document.querySelector('.return-to-home-link');
-                checkoutLink.innerHTML = newQuantity === 0 ? '' : newQuantity;
-    
+            
+            container.remove();
+            const newQuantity = getCartQuantity();
+            const checkoutLink = document.querySelector('.return-to-home-link');
+            checkoutLink.innerHTML = newQuantity === 0 ? '' : newQuantity;
+
+            renderPaymentSummary();
+
             
         });
     });
@@ -187,8 +189,10 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+
+            renderPaymentSummary();
         });
-    })
+    });
 
 }    
 
